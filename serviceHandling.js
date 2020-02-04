ca6e8a444a5b657a9f82a3ca558c2c0dfad61a86
vishal rana <vishal.rana@codezeros.com>
2020-02-04 19:52:38 +0530

    common file upload: adding thumbnail to all the images upload api

--- serviceHandling.js
+++ serviceHandling.js
@@ -1,5 +1,6 @@
 let cloudinary = require('cloudinary');
-let multerFile = require('./multerFile')
+let multerFile = require('./multerFile');
+const imageThumbnail = require('image-thumbnail');
 cloudinary.config({
     cloud_name: process.env.cloudinary_name,
     api_key: process.env.cloudinary_key,
@@ -7,7 +8,34 @@ cloudinary.config({
 });
 
 
+/**
+ * This function will create the thumbnail, update it on server and return us the url 
+ * @param {*} image_raw , image in raw format
+ */
+async function createThumbAndReturnUrl(image_raw) {
+    const thumbnail = await imageThumbnail({
+        uri: image_raw
+    });
+    return new Promise((resolve, reject) => {
+        let randomNum = `files/${Math.floor(Math.random() * 10000)}`;
+        require('fs').writeFile(randomNum, thumbnail, 'binary', async function () {
+            cloudinary.v2.uploader.upload(randomNum, (err_upper,data) => {
+                if (data) {
+                    require('fs').unlink(randomNum, function (err) {
+                        if (err) throw err;
+                        console.log('File deleted!');
+                    });
+                    resolve(data.url);
 
+                } else {
+                    console.log({err_upper})
+                    reject(err_upper);
+                }
+            })
+        });
+    })
+
+}
 function UploadFile(req, res) {
     multerFile.upload(req, res, async function (err) {
         if (err) {
@@ -22,17 +50,19 @@ function UploadFile(req, res) {
                 sendOption["resource_type"] = "raw"
             }
             cloudinary.v2.uploader.upload(`${process.cwd()}/files/${upload_len}`, sendOption, (error, result) => {
-                require('fs').unlink(`${process.cwd()}/files/${upload_len}`, (err_file, data_resp) => {
+                require('fs').unlink(`${process.cwd()}/files/${upload_len}`, async (err_file, data_resp) => {
                     if (error || err_file) {
                         return res.json({
                             code: 500,
                             message: "Internal server error, Please try again after some time."
                         })
                     } else {
+                        let thumb = await createThumbAndReturnUrl(result.url);
                         return res.json({
                             code: 201,
                             message: "File uploading successful.",
-                            url: result.url
+                            url: result.url,
+                            thumbnail: thumb
                         })
                     }
                 })
@@ -64,10 +94,10 @@ function UploadMultiFile(req, res) {
                     }
                     /**use coudinary for uploading images */
                     cloudinary.v2.uploader.upload(`${process.cwd()}/files/${data.name}`, sendOption, (error, result1) => {
-                        require('fs').unlink(`${process.cwd()}/files/${data.name}`, (err_file, data_resp) => {
+                        require('fs').unlink(`${process.cwd()}/files/${data.name}`, async (err_file, data_resp) => {
                             if (error || err_file) {
-                      console.log({error,err_file});
-				    return res.json({
+                                console.log({ error, err_file });
+                                return res.json({
                                     code: 500,
                                     message: "Internal server error, Please try again after some time."
                                 })
@@ -75,7 +105,8 @@ function UploadMultiFile(req, res) {
                                 if (result1) {
                                     /**push cloudinary url in upload_response array */
                                     /**check length of uploded url */
-                                    upload_response.push(result1.url)
+                                    let thumb = await createThumbAndReturnUrl(result1.url);
+                                    upload_response.push({ url: result1.url, thumbnail: thumb })
                                     if (upload_response.length === upload_len) {
                                         // return res.json({code:200,message:"Done"})
                                         resolve(upload_response)
